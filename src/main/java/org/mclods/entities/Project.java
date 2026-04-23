dc130b071d48d58747c5c733df4cf9b2cde9ad1a
Sagnik Chakraborty <47282664+mclods@users.noreply.github.com>
2026-04-24 00:39:05 +0530

    Added EmployeeDB entities and findAllEmployees

---
+++ src/main/java/org/mclods/entities/Project.java
@@ -0,0 +1,20 @@
+package org.mclods.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Project {
+    private String projectCode;
+
+    private String name;
+
+    private String client;
+
+    private String leadName;
+}
